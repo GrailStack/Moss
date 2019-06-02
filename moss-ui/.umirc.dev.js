54da8b792f652bad9f8fd7eab92a3541935a443c
VancySavoki <7021254@gmail.com>
2019-06-02 16:08:27 +0800

    前端 devServer 自动获取 apiHost 端口，打包自动复制

--- moss-ui/.umirc.dev.js
+++ moss-ui/.umirc.dev.js
@@ -1,6 +1,15 @@
+const yaml = require('js-yaml');
+const fs = require('fs');
+const applicationYml = '../moss-web/src/main/resources/config/application.yml';
+try {
+  var application = yaml.safeLoad(fs.readFileSync(applicationYml, 'utf8'));
+  console.log(application.server.port);
+} catch (e) {
+  console.log(e);
+}
 export default {
   define: {
     ENV: 'development',
-    apiHost: 'http://localhost:8080',
+    apiHost: 'http://localhost:' + application.server.port,
   }
 }
